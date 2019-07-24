935b28e70a0160aadf6567b79e3c740163518e22
jarrycyx <chengyx18@mails.tsinghua.edu.cn>
2019-07-24 11:28:24 +0800

    commenting

--- cpp-Components/articleslist.cpp
+++ cpp-Components/articleslist.cpp
@@ -1,15 +1,51 @@
+/************************************************************************************************************************
+类名：     ArticleList
+功能：     用于文章列表显示，继承QAbstractListModel，作为界面显示的Model
+备注：     该类用于多个界面，在外部调用构造函数时应指定Type，即是否允许进行更改操作
+                 实际因尽量减少在此类中直接进行修改操作，应该通过返回的指针在MyArticleObj中操作
+日期：     20190720 初步实现该类，与ListView联调
+                 20190721 进一步完善，增加全部删除等方法
+************************************************************************************************************************/
+
+//TO-DO: 使用类模板进行优化
+//TO-DO: 重载[]运算符
+
+
 #include "articleslist.h"
 
+/************************************************************************************************************************
+函数名：     ArticlesList
+功能：         构造函数
+参数：         int type，用于标注列表类型
+日期：         20190720 实现构造函数
+                     20190722 增加type属性
+************************************************************************************************************************/
 ArticlesList::ArticlesList(int type) : QAbstractListModel(), typeOfThisList(type)
 {
 }
 
+
+/************************************************************************************************************************
+函数名：     rowCount
+功能：         Qt要求重载函数，向界面返回总行数
+参数：         QModelIndex
+返回值：     总行数
+日期：         20190720
+************************************************************************************************************************/
 int ArticlesList::rowCount(const QModelIndex &parent) const
 {
     Q_UNUSED(parent);
     return articles.size();
 }
-
+/************************************************************************************************************************
+函数名：     data
+功能：         Qt要求重写函数，用于返回所需数据
+参数：         const QModelIndex &index, int role，后者用于标注所请求的数据变量名
+返回值：     QVariant
+日期：         20190720 实现title和content
+                     20190722 实现statusCode，type
+                     20190724 实现Translated各类信息
+************************************************************************************************************************/
 QVariant ArticlesList::data(const QModelIndex &index, int role) const
 {
     Q_UNUSED(role);
@@ -33,6 +69,14 @@ QVariant ArticlesList::data(const QModelIndex &index, int role) const
     }
 }
 
+/************************************************************************************************************************
+函数名：     roleNames
+功能：         Qt要求重写函数，用于返回各变量名对应序号的表
+返回值：     QHash
+日期：         20190720 实现title和content
+                     20190722 实现statusCode，type
+                     20190724 实现Translated各类信息
+************************************************************************************************************************/
 QHash<int, QByteArray> ArticlesList::roleNames() const
 {
     QHash<int, QByteArray> roles;
@@ -45,19 +89,30 @@ QHash<int, QByteArray> ArticlesList::roleNames() const
     return roles;
 }
 
+/************************************************************************************************************************
+函数名：     getArticle，editAnArticle
+功能：         获取article 的指针，用于获取信息或修改信息
+参数：         文章在列表中的index，以及各文章信息
+返回值：     QHash
+日期：         20190720 实现title和content
+                     20190722 实现statusCode，type
+                     20190724 实现Translated各类信息
+************************************************************************************************************************/
 MyArticleObj *ArticlesList::getArticle(int idx)
 {
     return articles[idx];
 }
 
+//虽然不建议在此类中直接编辑，但仍保留该函数接口
 void ArticlesList::editAnArticle(int index, QString title, QString content)
 {
     articles[index]->setTitleOfArticle(title);
     articles[index]->setContentOfArticle(content);
-    QModelIndex idx = createIndex(index, index);
-    emit dataChanged(idx, idx);
+    QModelIndex idx = createIndex(index, index);                                  //调用Qt接口生成所需Index
+    emit dataChanged(idx, idx);                                                                 //发送视图刷新信号
 }
 
+//实际用于通知视图刷新
 void ArticlesList::editAnArticle(int index)
 {
     QModelIndex idx = createIndex(index, index);
@@ -66,9 +121,9 @@ void ArticlesList::editAnArticle(int index)
 
 void ArticlesList::addAnArticle(MyArticleObj *newArticle)
 {
-    beginInsertRows(QModelIndex(), 0, 0);
+    beginInsertRows(QModelIndex(), 0, 0);                                                 //开始刷新
     articles.push_front(newArticle);
-    endInsertRows();
+    endInsertRows();                                                                                       //结束刷新
 }
 
 void ArticlesList::deleteAnArticle(int index)
--- cpp-Components/articleslist.h
+++ cpp-Components/articleslist.h
@@ -1,3 +1,20 @@
+//总体框架：             Storage - Data - Model - Interaction - View
+//该类所处层级：     Model
+/************************************************************************************************************************
+类名：     ArticleList
+功能：     用于文章列表显示，继承QAbstractListModel，作为界面显示的Model
+                 本类是沟通Model与Interaction的桥梁
+备注：     该类用于多个界面，在外部调用构造函数时应指定Type，即是否允许进行更改操作
+                 实际因尽量减少在此类中直接进行修改操作，应该通过返回的指针在MyArticleObj中操作
+                 该类的主要目的是通过Qt各对象和QML视图直接交互
+日期：     20190720 初步实现该类，与ListView联调
+                 20190721 进一步完善，增加全部删除等方法
+************************************************************************************************************************/
+
+//TO-DO: 使用类模板进行优化
+//TO-DO: 重载[]运算符
+
+
 #ifndef ARTICLESLIST_H
 #define ARTICLESLIST_H
 
@@ -8,23 +25,29 @@
 class ArticlesList : public QAbstractListModel
 {
 public:
+    //构造函数中必须指定Type，为1表示自己的文章，可以进行删除更改等操作
     explicit ArticlesList(int type);
+    //Qt要求重载的方法，用于计算总的行数
     int rowCount(const QModelIndex &parent = QModelIndex()) const override;
+    //Qt要求重载的方法，用于查询某个数据
     QVariant data(const QModelIndex &index, int role = Qt::DisplayRole) const override;
 
+    //增加文章
     void addAnArticle(MyArticleObj *newArticle);
+    //修改文章，传入新的文章和内容，同时通知界面进行更改
     void editAnArticle(int index, QString title, QString content);
     void editAnArticle(int index);
+    //删除文章，同时通知界面进行更改
     void deleteAnArticle(int index);
-
+    //删除所有项
     inline void removeAllArticles() { articles.clear(); }
-
+    //获取文章的指针，用于实际的修改操作
     MyArticleObj *getArticle(int idx);
 
-    Q_INVOKABLE void removeOne();
-
 protected:
+    //存储所有文章的数据结构，由于需要保证文章对象实体只有一份，使用指针
     QVector<MyArticleObj *> articles;
+    //存储QML界面需要查询的变量名
     QHash<int, QByteArray> roleNames() const override;
     int typeOfThisList;
 };
